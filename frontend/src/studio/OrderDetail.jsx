0df8ef539d7c9445a299bf261c7ad470628a3c64
Jin <thaonhiaries@gmail.com>
2023-12-05 23:33:25 +0700

    studio-order-detail: edit popup

--- frontend/src/studio/OrderDetail.jsx
+++ frontend/src/studio/OrderDetail.jsx
@@ -17,11 +17,31 @@ import {
   FormControl,
   Select,
   MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { MdEdit, MdDeleteOutline } from "react-icons/md";
+import { RiSubtractFill } from "react-icons/ri";
+import { IoIosAdd } from "react-icons/io";
+import { FaXmark } from "react-icons/fa6";
 
 function OrderDetail() {
+  // Edit
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   // selection
   const [value, setValue] = useState("");
   const handleChange = (event) => {
@@ -80,19 +100,17 @@ function OrderDetail() {
           <TableCell align="left">{row.quantity}</TableCell>
           <TableCell align="left">{row.price}</TableCell>
           <TableCell align="left">
-            <Button
-              variant="text"
-              sx={{
-                color: "#3F41A6",
-                textTransform: "none",
-                "&:hover": {
-                  bgcolor: "#F6F5FB",
-                  borderRadius: "43px",
-                },
-              }}
-            >
-              Đánh giá
-            </Button>
+            <IconButton>
+              <MdEdit
+                onClick={handleClickOpen}
+                style={{ width: "22px", height: "22px", color: "#C5CEE0" }}
+              />
+            </IconButton>
+            <IconButton>
+              <MdDeleteOutline
+                style={{ width: "22px", height: "22px", color: "#C5CEE0" }}
+              />
+            </IconButton>
           </TableCell>
         </TableRow>
         <TableRow>
@@ -204,6 +222,104 @@ function OrderDetail() {
           </div>
         </div>
       </TableContainer>
+
+      {/* Dialog Edit */}
+      <Dialog open={open} onClose={handleClose}>
+        <div className="max-w-[400px] border border-[color:var(--gray-scale-gray-100,#E6E6E6)] bg-white flex flex-col items-stretch pb-10 rounded-lg border-solid">
+          <div className="shadow-sm bg-white flex items-center justify-end w-full gap-16 pl-20 pr-2 py-4 rounded-lg ">
+            <div className="text-indigo-800 text-2xl font-semibold leading-9 whitespace-nowrap">
+              Tùy chỉnh
+            </div>
+            <IconButton onClick={handleClose}>
+              <FaXmark />
+            </IconButton>
+          </div>
+          <div className="flex w-full justify-start gap-4 mt-3 pl-5">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/dd7e23482da9be4fe42570372d7f1c5c8f20adafcb759e4ea962934e54566ac9?apiKey=a8bdd108fb0746b1ab1fa443938e7c4d&"
+              className="aspect-square object-contain object-center w-[60px] overflow-hidden shrink-0 max-w-full"
+            />
+            <div className=" flex grow basis-[0%] flex-col py-1">
+              <div className="justify-center text-yellow-950 text-lg font-semibold">
+                Chụp ảnh gia đình
+              </div>
+              <div className="z-[1] flex justify-start gap-5 mt-2.5 -mb-1.5 pr-20 items-start max-md:pr-5">
+                <div className="w-18 h-7 text-indigo-800 text-sm leading-5 whitespace-nowrap justify-center items-stretch rounded bg-violet-50 self-stretch aspect-[2.3448275862068964] px-2 py-1">
+                  Dịch vụ
+                </div>
+                <div className="w-18 h-7 text-indigo-800 text-sm leading-5 whitespace-nowrap justify-center items-stretch rounded bg-violet-50 self-stretch aspect-[2.3448275862068964] px-2 py-1">
+                  Gia đình
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full flex-col mt-5 pl-5">
+            <div className="flex items-center gap-6">
+              <div className="justify-center flex flex-col">
+                <div className="text-zinc-900 text-sm leading-5">Giá (VNĐ)</div>
+                <TextField
+                  id="outlined-basic"
+                  variant="outlined"
+                  placeholder="500000"
+                  sx={{
+                    marginTop: "5px",
+                    width: "100px",
+                    boxSizing: "border-box",
+                  }}
+                />
+                {/* <div className="text-stone-500 text-xs font-light tracking-wide whitespace-nowrap justify-center items-stretch border-[color:var(--Border,#EAEAEA)] mt-1.5 px-14 py-4 rounded-md border-2 border-solid max-md:px-5">
+                  500,000
+                </div> */}
+              </div>
+              <div className="justify-center items-center border border-[color:var(--gray-scale-gray-100,#E6E6E6)] bg-white flex gap-0 p-2 rounded-[170px] border-solid self-end">
+                <div className="bg-zinc-100 flex w-[20px] shrink-0 h-[20px] flex-col rounded-[170px] items-center justify-center">
+                  <IconButton color="primary">
+                    <RiSubtractFill
+                      style={{
+                        color: "#666666",
+                        width: "15px",
+                        height: "15px",
+                      }}
+                    />
+                  </IconButton>
+                </div>
+                <div className="text-zinc-900 text-center text-sm leading-6 mx-2">
+                  5
+                </div>
+                <div className="bg-zinc-100 flex w-[20px] shrink-0 h-[20px] flex-col rounded-[170px] items-center justify-center">
+                  <IconButton color="primary">
+                    <IoIosAdd
+                      style={{
+                        color: "#666666",
+                        width: "15px",
+                        height: "15px",
+                      }}
+                    />
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+            <Button
+              variant="contained"
+              sx={{
+                marginTop: "50px",
+                marginX: "auto",
+                textTransform: "none",
+                borderRadius: "43px",
+                color: "#F6F5FB",
+                bgcolor: "#3F41A6",
+                width: "130px",
+                "&:hover": {
+                  bgcolor: "#3F41A6B2",
+                },
+              }}
+            >
+              Lưu thay đổi
+            </Button>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 }
