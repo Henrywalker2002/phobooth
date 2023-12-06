abee60ce0a15b6d28511bb649cb754f72232f274
Hung Nguyen <davisdavis448@gmail.com>
2023-12-06 16:31:52 +0700

    add jwt middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,14 +63,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'backend.custom_middleware.CustomMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-<<<<<<< Updated upstream
-=======
     'backend.JWTMiddleware.JWTAuthenticationMiddleware',
     'backend.custom_middleware.CustomMiddleware',
->>>>>>> Stashed changes
 ]
 
 ROOT_URLCONF = 'backend.urls'
