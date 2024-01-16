8ca451b87008be138fd5c260f7474f5d4c29f2fd
Henrywalker2002 <davisdavis448@gmail.com>
2024-01-16 21:40:58 +0700

    hot fix

--- backend/backend/JWTMiddleware.py
+++ backend/backend/JWTMiddleware.py
@@ -1,10 +1,11 @@
 import jwt
-import traceback
 
 from django.utils.functional import SimpleLazyObject
 from django.utils.deprecation import MiddlewareMixin
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import AnonymousUser
+from user.models import User
 from django.contrib.auth.middleware import get_user
+from backend.settings import SECRET_KEY
 
 
 class JWTAuthenticationMiddleware(MiddlewareMixin):
@@ -14,21 +15,28 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
     @staticmethod
     def get_jwt_user(request):
 
-        user_jwt = get_user(request)
+        try:
+            user_jwt = get_user(request) 
+        except Exception as e:
+            user_jwt = AnonymousUser()
         if user_jwt.is_authenticated:
             return user_jwt
         token = request.META.get('HTTP_AUTHORIZATION', None)
+        if token:
+            token = token.split()[1]
 
         user_jwt = AnonymousUser()
         if token is not None:
             try:
                 user_jwt = jwt.decode(
                     token,
-                    None,
+                    SECRET_KEY, 
+                    algorithms=['HS256']
                 )
                 user_jwt = User.objects.get(
-                    id=user_jwt['data']['user']['id']
+                    id=user_jwt["user_id"]
                 )
             except Exception as e: # NoQA
-                traceback.print_exc()
+                # traceback.print_exc()
+                return user_jwt
         return user_jwt
