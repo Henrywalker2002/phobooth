f23de5708f12c58d49d491ac9df9355b50ea632e
Jin <thaonhiaries@gmail.com>
2024-04-15 23:36:18 +0700

    update UI demo

--- frontend/src/studio/demo/Album.jsx
+++ frontend/src/studio/demo/Album.jsx
@@ -4,7 +4,7 @@ import {
   Button,
   ImageList,
   ImageListItem,
-  Badge, 
+  Badge,
   IconButton,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
@@ -16,11 +16,10 @@ import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 function Album({
   imageList,
   setImageList,
-  currentDemo,
   setCurrentDemo,
   narbarType,
   order_id,
-  page, 
+  page,
   setPage,
   totalImage,
   setTotalImage,
@@ -31,30 +30,35 @@ function Album({
 
   useEffect(() => {
     const handleScroll = () => {
-      const { scrollHeight, scrollTop, clientHeight } = scrollContainerRef.current;
+      const { scrollHeight, scrollTop, clientHeight } =
+        scrollContainerRef.current;
       const isBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
-  
+
       if (isBottom) {
-        console.log('Scrolled to the bottom');
+        console.log("Scrolled to the bottom");
         // Load more images or do something else
         setPage(page + 1);
       }
     };
-  
+
     const scrollContainer = scrollContainerRef.current;
-    scrollContainer.addEventListener('scroll', handleScroll);
+    scrollContainer.addEventListener("scroll", handleScroll);
 
     return () => {
-      scrollContainer.removeEventListener('scroll', handleScroll);
+      scrollContainer.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   const handleDeleteImg = (item) => {
-    axiosPrivate.delete(`/demo/${item.id}/`).then((res) => {
-      setImageList(imageList.filter((img) => img.id !== item.id));
-    }).then((err) => {
-      console.log(err);
-    });
+    axiosPrivate
+      .delete(`/demo/${item.id}/`)
+      .then((res) => {
+        setImageList(imageList.filter((img) => img.id !== item.id));
+        setTotalImage(totalImage - 1);
+      })
+      .then((err) => {
+        console.log(err);
+      });
   };
   return (
     <Paper
@@ -63,14 +67,13 @@ function Album({
         width: "fit-content",
         margin: "40px 30px",
         minWidth: "300px",
-        maxHeight : "700px",
-        overflowY : "scroll",
-        overflowX : "hidden",
+        height: "800px",
+
         border: "1px solid #d6d3d1",
       }}
     >
       <div className="flex justify-between items-center px-4 py-2 bg-white rounded-lg leading-[150%]">
-        {narbarType === "studio" &&
+        {narbarType === "studio" && (
           <Button
             variant="text"
             startIcon={<AddIcon />}
@@ -85,7 +88,7 @@ function Album({
           >
             Thêm hình ảnh
           </Button>
-        }
+        )}
         <div className="flex gap-1 text-base">
           <div className="text-zinc-900">Tổng số :</div>
           <div className="text-stone-500">{totalImage}</div>
@@ -94,21 +97,27 @@ function Album({
       <Divider />
 
       <ImageList
-        sx={{ width: "fit-content", height: "fit-content", padding: "17px" }}
+        sx={{
+          width: "fit-content",
+          height: "92%",
+          padding: "17px",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          "::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
         cols={2}
-        gap={12}
+        gap={20}
+        rowHeight={120}
       >
         {imageList?.map((item) => (
           <ImageListItem
             key={item.id}
             sx={{
-              width: "138px",
-              height: "105px",
+              width: "135px",
               border: "1px solid #E0E0E0",
               borderRadius: "5px",
-              "& .MuiImageListItem-img": {
-                height: "119px",
-              },
             }}
           >
             <Badge
@@ -123,15 +132,12 @@ function Album({
               }
               sx={{
                 bgcolor: "transparent",
-                height: "108px",
-
+                maxHeight: "90px",
               }}
             >
               <img
-                width="108"
-                height="89"
-                className="rounded-[5px]"
-                src={`${item.image?.thumbnail}`}
+                className="w-full h-[120px] rounded object-cover"
+                src={item.image?.thumbnail}
                 alt={item.title}
                 loading="lazy"
                 onClick={() => setCurrentDemo(item)}
